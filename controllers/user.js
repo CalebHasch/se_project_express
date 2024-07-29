d124501f25784dc9e4b3676c294ed68334e46a00
Caleb <chasch96@gmail.com>
2024-07-29 21:29:37 +0800

    add email and password to user model

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const bycrypt = require("bycryptjs");
+
 const User = require("../models/user");
 const { invalidData, notFound, defaultError } = require("../utils/error");
 
@@ -32,16 +34,19 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, avatar } = req.body;
-
-  User.create({ name, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        res.status(invalidData.status).send({ message: invalidData.message });
-      } else {
-        res.status(defaultError.status).send({ message: defaultError.message });
-      }
-    });
+  const { name, avatar, email, password } = req.body;
+  bycrypt.hash(password, 10).then((hash) => {
+    User.create({ name, avatar, email, hash })
+      .then((user) => res.send({ data: user }))
+      .catch((err) => {
+        console.error(err);
+        if (err.name === "ValidationError") {
+          res.status(invalidData.status).send({ message: invalidData.message });
+        } else {
+          res
+            .status(defaultError.status)
+            .send({ message: defaultError.message });
+        }
+      });
+  });
 };
